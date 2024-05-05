96cb0c61b48a87877a6bbccecbffcd0730d364c5
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-05-05 11:51:19 +0200

    completed queen death system

--- game/src/systems/handlers/queen_death/v_1.rs
+++ game/src/systems/handlers/queen_death/v_1.rs
@@ -1,5 +1,6 @@
 use enum_as_inner::EnumAsInner;
 use thiserror::Error;
+use crate::entities::specialist;
 
 use crate::entities::world::World;
 use crate::systems::{SystemHandler, SystemHandlingError};
@@ -27,6 +28,49 @@ impl SystemHandler for Handler {
         "queen_death/v_1".to_string()
     }
     fn handle(&self, world: &mut World) -> Result<(), SystemHandlingError> {
-        todo!()
+
+        let keys = world.specialists.keys();
+
+        for id in keys {
+            let value = world.specialists.get(id);
+
+            let spec = match value {
+                Some(spec) => spec,
+                None => continue
+            };
+
+            let specialist::SpecialistOwner::PlayerOwned(owner_id) = spec.owner else {
+                continue;
+            };
+
+            if !spec.variant.is_queen() {
+                continue;
+            }
+
+            let owns_location = match spec.location {
+                specialist::SpecialistLocation::Outpost(outpost_id) => {
+                    let outpost = world.outposts.get(*outpost_id);
+                    let outpost_owned_player = outpost.map(|outpost| outpost.owner.as_player_owned()).flatten();
+                    outpost_owned_player.map(|outpost_owner_id| *outpost_owner_id == owner_id).unwrap_or(false)
+                },
+                specialist::SpecialistLocation::Ship(ship_id) => {
+                    let ship = world.ships.get(*ship_id);
+                    let ship_owned_player = ship.map(|ship| ship.owner.as_ship_player_owned()).flatten();
+                    ship_owned_player.map(|ship_owner_id| *ship_owner_id == owner_id).unwrap_or(false)
+                },
+                specialist::SpecialistLocation::Unknown => continue
+            };
+
+            if owns_location {
+                continue;
+            }
+
+            let specialist_id = id.to_owned();
+            let specialist = world.specialists.remove(*specialist_id).unwrap();
+            world.dead_specialists.insert(specialist_id, specialist);
+        }
+
+
+        Ok(())
     }
 }
