7c1406b7f96c2b7339133bc75672960456e013f7
JulianBuse <44533409+JulianBuse@users.noreply.github.com>
2024-05-11 21:11:38 +0200

    started work on worker

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,13 +1,11 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(
-    _req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<HttpResponse> {
-    console_error_panic_hook::set_once();
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(Body::empty())?)
+async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    Router::new()
+        .get_async("/hello", |_req, _ctx| async move {
+            Response::error("Not found lol", 404)
+        })
+        .run(req, env)
+        .await
 }
